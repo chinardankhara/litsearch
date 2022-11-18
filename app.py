51c4870760f89741ed0488914b5843a1d1aa85b0
ChinDanCD <68681680+ChinDanCD@users.noreply.github.com>
2022-11-18 13:24:59 -0500

    added arxiv search support

--- app.py
+++ app.py
@@ -7,29 +7,45 @@ st.set_page_config(page_title="Lit Search", layout = "wide")
 
 st.markdown('<h1 style="text-align: center;">Literature Search</h1>', unsafe_allow_html=True)
 st.markdown('<br>', unsafe_allow_html=True)
-type_menu = som.option_menu(None, ["Search by DOI", "Search by Topic"],
- icons = ['123', 'collection'], default_index=0, orientation="horizontal")
+type_menu = som.option_menu(None, ["Search by DOI", "Search Author"],
+ icons = ['123', 'person'], default_index=0, orientation="horizontal")
 
-def display_search_by_doi():
-    #add radio button for refereced and related works
-    type = st.radio("Type of results", ["Referenced Works", "Related Works"], horizontal=True,
+def display_search_by_id():
+    #create twto columns for result and id radio buttons
+    col1, col2 = st.columns(2)
+    with col1:
+        result_type = st.radio("Type of results", ["Referenced Works", "Related Works"], horizontal=True,
      help="Referenced works are works that are cited by the given DOI. Related works are works that are similar to the given DOI - generated algorithmically.")
-    #put a text input box
-    doi = st.text_input('Search DOI',
-     help = "Returns works related to the input paper")
-
-    #some doi handling  
-    if doi and "doi.org/" not in doi:
-        doi = "doi/" + doi
-    if doi and not doi.startswith('https://'):
-        doi = 'https://' + doi
+    with col2:
+        id_type = st.radio("Type of ID", ["DOI", "Arxiv"], horizontal=True,
+     help="DOI is the Digital Object Identifier. Arxiv is the Arxiv ID.")
     
+    id = st.text_input('Search ID',
+     help = "Returns works related to the input paper").strip()
+    if id and "doi.org/" not in id:
+        id = "doi/" + id
+    if id and not id.startswith('https://'):
+        id = 'https://' + id
+
+
     try:
-        data = oa.get_results_from_doi(doi, key = type)
+        data = oa.get_results_from_id(id, result_type = result_type, id_type = id_type)
         data = df_to_aggrid(data)
     except:
-        if doi: st.markdown('<p style="color:white; font-weight:bold;">Invalid DOI</p>',
-         unsafe_allow_html=True)
+        if id:
+            st.markdown('<p style="color:white; font-weight:bold;">Invalid ID. The application is under development' +
+             '- so it might be a system fault. Thanks for your patience.</p>', unsafe_allow_html=True)
+
+def display_search_author():
+    #add radio button for profile and works
+    # type = st.radio("Type of results", ["Profile", "Works"], horizontal=True,
+    #  help="Profile returns information about the author. Works returns works by the author.")
+    # name = st.text_input('Search Authors',
+    #  help = "If things don't work, please try putting exact name of the author").strip()
+    #coming soon
+    st.markdown('<h3 style="color:white; font-weight:bold;">Coming soon</h3>', unsafe_allow_html=True)
+
+    
 
 def df_to_aggrid(df):
         #TODO: make this function configurable
@@ -40,8 +56,12 @@ def df_to_aggrid(df):
         df = AgGrid(df, gridOptions=gridOptions)
         return df
 
-#TODO: add search by topic code
-display_search_by_doi()
+
+if type_menu == "Search by DOI":
+    display_search_by_id()
+elif type_menu == "Search Author":
+    display_search_author()
+
 
 #hide streamlit footer
 hide_streamlit_style = """ <style> footer {visibility: hidden;} </style> """
